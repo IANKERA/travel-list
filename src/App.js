ca06613fa2cdfd39ac4594bd1ffc4867af213e26
IANKERA <giannhskeram@gmail.com>
2023-07-26 16:45:23 +0300

    add items to the list

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ const initialItems = [
   { id: 3, description: "Charger", quantity: 1, packed: true },
 ];
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -20,7 +26,7 @@ function Logo() {
   return <h1>🌴 Far Away 🌴</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -31,6 +37,8 @@ function Form() {
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
     console.log(newItem);
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -60,11 +68,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
